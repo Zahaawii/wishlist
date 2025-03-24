cd420d970ca5e04c0b2699c5cd07cf754d331182
Zahaawii <Zahaawii@gmail.com>
2025-03-24 11:48:18 +0100

    test, fixes #3

--- src/main/java/apiassignment/wishlist/controller/WishlistController.java
+++ src/main/java/apiassignment/wishlist/controller/WishlistController.java
@@ -2,6 +2,7 @@ package apiassignment.wishlist.controller;
 
 import apiassignment.wishlist.service.WishlistService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WishlistController {
@@ -10,6 +11,11 @@ public class WishlistController {
 
     public WishlistController(WishlistService wishlistService) {
         this.wishlistService = wishlistService;
+    }
+
+    @GetMapping("/test")
+    public String homepage(){
+        return "homepage";
     }
 
 }
