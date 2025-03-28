253eb943c05d59889ec9bd93a1d9aea114189f0d
huw02 <hannibal@ussing.com>
2025-03-28 14:15:30 +0100

    desperation

--- src/main/java/apiassignment/wishlist/service/WishlistService.java
+++ src/main/java/apiassignment/wishlist/service/WishlistService.java
@@ -71,9 +71,6 @@ public class WishlistService{
         return wishlistRepository.updateUser(user);
     }
 
-    public User updateUser(User user){
-        return wishlistRepository.updateUser(user);
-    }
 
 
 
